41cb14d9f73d30038235beae5908897c8fb40ec5
Dennis-mf <dennismarroquin099@gmail.com>
2025-08-04 21:38:50 -0600

    Add email template file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { Resend } from 'resend';
+import * as fs from 'fs';
+import * as path from 'path';
 import dotenv from "dotenv";
 dotenv.config();
 const resend = new Resend(process.env.RESEND_API);
@@ -7,11 +9,15 @@ const email = process.env.EMAIL;
 console.log("resend token api: ", resend);
 
 (async function () {
+
+    const templatePath = path.join("./", "email-template-large.html"); //ruta de la plantilla
+    const templateContent = fs.readFileSync(templatePath, 'utf-8'); //leemos el archivo html
+
   const { data, error } = await resend.emails.send({
     from: 'Testing Company <onboarding@resend.dev>',
     to: [email],
     subject: 'This is a test',
-    html: '<strong>It works!</strong>',
+    html: templateContent,
   });
 
   if (error) {

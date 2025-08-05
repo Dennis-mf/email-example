83d49dff1179892e8378d3d3d97938d05e0b3973
Dennis-mf <dennismarroquin099@gmail.com>
2025-08-04 21:12:25 -0600

    Add receiver email param

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ import { Resend } from 'resend';
 import dotenv from "dotenv";
 dotenv.config();
 const resend = new Resend(process.env.RESEND_API);
+const email = process.env.EMAIL;
 
 console.log("resend token api: ", resend);
 
 (async function () {
   const { data, error } = await resend.emails.send({
     from: 'Testing Company <onboarding@resend.dev>',
-    to: ['dennismarroquin099@gmail.com'],
-    subject: 'Hello World',
+    to: [email],
+    subject: 'This is a test',
     html: '<strong>It works!</strong>',
   });
 
